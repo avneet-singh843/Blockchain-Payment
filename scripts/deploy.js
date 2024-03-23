ae25682c1ab1b2a4840ec123af0f7a48a50d83d8
Avneet Singh <avneetsingh843@gmail.com>
2024-03-23 22:04:57 +0530

    Smart contract Deployed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -15,4 +15,4 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-//0x5FbDB2315678afecb367f032d93F642f64180aa3
+//Deploy address - 0x5FbDB2315678afecb367f032d93F642f64180aa3
